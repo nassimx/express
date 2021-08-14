9bbda85cdf09961e13bfe0b293e9cbf6a9f4b4c2
nassimx <mr.nassimfr@gmail.com>
2021-08-14 16:10:17 +0100

    express

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const users = require('./users')
+
+var app = express();
+const fs = require('fs')
+
+app.get('/home', (req, res) => {
+    res.send()
+})
+
+// middleware
+// const messages = (req, res, next) => {
+//     console.log("hello")
+//     // next()
+// }
+
+function timework(req, res, next) {
+    var date = new Date();
+    var today = date.getDay();
+    var time = date.getHours();
+    console.log(today);
+    console.log(time);
+    if (1 <= today && today <= 5 && 9 <= time && time <= 17) {
+        next();
+    } else {
+        res.send("Close. Come Back later");
+    }
+}
+
+
+// app.get('/users', messages, (req, res) => {
+//     res.send(users)
+// })
+
+// routing
+app.get("/", (req, res) => {
+    fs.readFile('./public/index.html', 'utf-8', (err, data) => {
+        err ? console.log(err) : res.send(data)
+    })
+})
+
+app.get("/services", (req, res) => {
+    fs.readFile('./public/services.html', 'utf-8', (err, data) => {
+        err ? console.log(err) : res.send(data)
+    })
+})
+
+app.get("/contactus", (req, res) => {
+    fs.readFile('./public/contactus.html', 'utf-8', (err, data) => {
+        err ? console.log(err) : res.send(data)
+    })
+})
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, (err) => err ? console.log(err) : console.log(`server running on port ${PORT}`))
---
+++ users.js
@@ -0,0 +1,17 @@
+const users = [{
+    id: 0,
+    name: "mohamed",
+    job: "web developer"
+},
+{
+    id: 1,
+    name: "rami",
+    job: "enginer",
+}, {
+    id: 2,
+    name: "ahmed",
+    job: "try"
+}
+];
+
+module.exports = users;
